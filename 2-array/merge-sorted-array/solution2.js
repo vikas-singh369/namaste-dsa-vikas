418ca3005bec7a3ca13016f148a0f5f74948fdd2
Vikas Singh <vikashverma3001@gmail.com>
2026-01-07 15:53:29 +0530

    refactor: improve condition checks and formatting in merge functions

--- 2-array/merge-sorted-array/solution2.js
+++ 2-array/merge-sorted-array/solution2.js
@@ -7,7 +7,7 @@ function merge2(nums1, m, nums2, n) {
   let p2 = 0;
 
   for (let i = 0; i < m + n; i++) {
-    if ((p1 < m && nums1Copy[p1] < nums2[p2]) || p2 >= n) {
+    if (p2 >= n || (p1 < m && nums1Copy[p1] < nums2[p2])) {
       nums1[i] = nums1Copy[p1];
       p1++;
     } else {
--- 2-array/merge-sorted-array/solution3.js
+++ 2-array/merge-sorted-array/solution3.js
@@ -1,16 +1,16 @@
-
 /**
  * *Approach 3: Space Optimized filled the array in reverse order so no extra space used
- * *Time Complexity O(m+n log m+n)
- * *Space Complexity O(1)
  */
 
-function merge3(nums1, m, nums2, n) {
+var merge = function (nums1, m, nums2, n) {
+  // pointers
   let p1 = m - 1;
   let p2 = n - 1;
 
-  for (let i = m + n - 1; i >= 0; i--) {
-    if (p2 < 0) break;
+  for (let i = nums1.length - 1; i >= 0; i--) {
+    if (p2 < 0) {
+      break;
+    }
 
     if (p1 >= 0 && nums1[p1] > nums2[p2]) {
       nums1[i] = nums1[p1];
@@ -20,4 +20,4 @@ function merge3(nums1, m, nums2, n) {
       p2--;
     }
   }
-}
+};
