018d1af65fbacf845a09631c3ffd3d6cfd6083e4
Vikas Singh <vikashverma3001@gmail.com>
2026-01-08 17:43:16 +0530

    optimal approach

--- 2-array/single-number/solution2.js
+++ 2-array/single-number/solution2.js
@@ -1,10 +1,10 @@
 // 2 optimal Approach
 var singleNumber = function (nums) {
-  let xor = 0;
+  let single = 0;
 
-  for (let i = 0; i < nums.length; i++) {
-    xor = xor ^ nums[i];
+  for (const value of nums) {
+    single = single ^ value;
   }
 
-  return xor;
+  return single;
 };
