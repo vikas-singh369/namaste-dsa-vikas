16c391534fc42735c536714b1dadd8bf1212c4dd
Vikas Singh <vikashverma3001@gmail.com>
2026-02-03 18:42:46 +0530

    refactor: simplify variable assignments and improve readability in addTwoNumbers function

--- 5-linked-list/add-two-numbers/solution.js
+++ 5-linked-list/add-two-numbers/solution.js
@@ -1,24 +1,22 @@
 var addTwoNumbers = function (l1, l2) {
   let headPoint = new ListNode();
   let curr = headPoint;
-
-  let sum = 0;
-  let digit = 0;
   let carry = 0;
 
-  while (l1 || l2 || carry != 0) {
-    let l1Value = l1 != null ? l1.val : 0;
-    let l2Value = l2 != null ? l2.val : 0;
+  while (l1 || l2 || carry) {
+    let firstValue = l1 ? l1.val : 0;
+    let secondValue = l2 ? l2.val : 0;
 
-    sum = l1Value + l2Value + carry;
-    digit = sum % 10;
+    let sum = firstValue + secondValue + carry;
+
+    let digit = sum % 10;
     carry = Math.floor(sum / 10);
 
     curr.next = new ListNode(digit);
     curr = curr.next;
 
-    l1 = l1 && l1.next;
-    l2 = l2 && l2.next;
+    l1 = l1 ? l1.next : null;
+    l2 = l2 ? l2.next : null;
   }
 
   return headPoint.next;
