759cb2bf9c67adb6d454c470e2391806dfa90446
Vikas Singh <vikashverma3001@gmail.com>
2026-03-31 17:49:04 +0530

    add: rotten oranges solution

--- 7-stack-and-queue/rotten-oranges/sulution.js
+++ 7-stack-and-queue/rotten-oranges/sulution.js
@@ -1,71 +1,53 @@
-/**
- * @param {number[][]} grid
- * @return {number}
- */
 var orangesRotting = function (grid) {
+  let row = grid.length;
+  let col = grid[0].length;
+  let queue = [];
+  let maxMin = 0;
 
-    let m = grid.length;
-    let n = grid[0].length;
-    let queue = [];
+  // add all rotten in qeues
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] === 2) {
+        queue.push([i, j, 0]);
+      }
+    }
+  }
 
-    // 1 step traver the whole grid and find rotten oranges push into queue
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 2) {
-                queue.push([i, j, 0]);
-            }
-        }
+  // add all adjecent
+
+  while (queue.length) {
+    let [x, y, mintue] = queue.shift();
+
+    if (x > 0 && grid[x - 1][y] === 1) {
+      grid[x - 1][y] = 2;
+      queue.push([x - 1, y, mintue + 1]);
     }
 
-    let maxMin = 0;
-
-    // make all rotten adjesent rotten and push index into queue and make sure boundaries
-
-    while (queue.length) {
-        // console.table(grid);
-        let [x, y, level] = queue.shift();
-        // console.log("Processing:", x, y, "at level", level);
-
-
-        // left side checking ( row -1 )
-        if (x > 0 && grid[x - 1][y] === 1) {
-            grid[x - 1][y] = 2;
-            queue.push([x - 1, y, level + 1])
-        }
-
-        // right side checking ( row + 1)
-        if (x < m - 1 && grid[x + 1][y] === 1) {
-            grid[x + 1][y] = 2;
-            queue.push([x + 1, y, level + 1])
-        }
-
-        // top side checking ( col - 1 )
-        if (y > 0 && grid[x][y - 1] === 1) {
-            grid[x][y - 1] = 2;
-            queue.push([x, y - 1, level + 1])
-        }
-
-        // bottom side checking ( col + 1)
-        if (y < n - 1 && grid[x][y + 1] === 1) {
-            grid[x][y + 1] = 2;
-            queue.push([x, y + 1, level + 1]);
-        }
-
-
-        maxMin = Math.max(level, maxMin);
+    if (x < row - 1 && grid[x + 1][y] === 1) {
+      grid[x + 1][y] = 2;
+      queue.push([x + 1, y, mintue + 1]);
     }
 
-
-    // traverse the whole grid ones again find fresh orange if fresh oranage remaining return -1 
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (grid[i][j] === 1) {
-                return -1;
-            }
-        }
+    if (y > 0 && grid[x][y - 1] === 1) {
+      grid[x][y - 1] = 2;
+      queue.push([x, y - 1, mintue + 1]);
+    }
+    if (y < col - 1 && grid[x][y + 1] === 1) {
+      grid[x][y + 1] = 2;
+      queue.push([x, y + 1, mintue + 1]);
     }
 
-    // other wise return mitues that take to whole grid oranges rotten 
+    maxMin = Math.max(mintue, maxMin);
+  }
 
-    return maxMin;
+  // final check if any fresh orange remaining
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] === 1) {
+        return -1;
+      }
+    }
+  }
+
+  return maxMin;
 };
